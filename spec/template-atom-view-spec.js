a72d954cb8b44fa7888afcebe82a15050263d8a8
cforno12 <cforno1@binghamton.edu>
2017-02-25 13:47:35 -0500

    package files

---
+++ spec/template-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TemplateAtomView from '../lib/template-atom-view';
+
+describe('TemplateAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
